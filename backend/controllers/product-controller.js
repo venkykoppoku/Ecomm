13874cf3f51d062a4668719b6cb46d073ea023e4
venkykoppoku <venky94.koppoku@gmail.com>
2025-08-13 15:08:03 +0530

    update getAll products controller

--- backend/controllers/product-controller.js
+++ backend/controllers/product-controller.js
@@ -1,15 +1,14 @@
 import Product from "../models/product.js";
 
 // get all products /api/v1/products
-export const getAllProducts = (req, res) => {
-  return res.status(200).json({
-    message: "All products",
-  });
+export const getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  return res.status(200).json({ products });
 };
 
 // create the product /api/v1/admin/products
 export const createProduct = async (req, res) => {
   console.log();
   const product = await Product.create(req.body.product);
-  return res.status(200).json(product);
+  return res.status(200).json({ product });
 };
