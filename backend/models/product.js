c5faa535d0079f3a2d49762a5929b2076d967a21
venkykoppoku <venky94.koppoku@gmail.com>
2025-08-13 08:57:42 +0530

    create product model

---
+++ backend/models/product.js
@@ -0,0 +1,92 @@
+import mongoose from "mongoose";
+
+const productScheema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter product name"],
+      maxLength: [200, "product name length not exceed 200 charecters"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please enter product price"],
+      maxLength: [5, "product price length not exceed 5 digits"],
+    },
+    description: {
+      type: String,
+      required: [true, "Please enter product description"],
+    },
+    stock: {
+      type: Number,
+      required: [true, "Please enter product stock"],
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    images: [
+      {
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    category: {
+      type: String,
+      required: [true, "Please enter product category"],
+      enum: {
+        values: [
+          "Electronics",
+          "Cameras",
+          "Laptops",
+          "Accessories",
+          "Headphones",
+          "Food",
+          "Books",
+          "Sports",
+          "Outdoor",
+          "Home",
+        ],
+        message: "Please select correct category",
+      },
+    },
+    sellar: {
+      type: String,
+      required: [true, "Please enter product seller"],
+    },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
+    reviews: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        rating: {
+          type: Number,
+          required: true,
+        },
+        comment: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", productScheema);
