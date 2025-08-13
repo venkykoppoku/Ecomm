3a73200d00478aeea6ba40e8aef805fc47c3acd6
venkykoppoku <venky94.koppoku@gmail.com>
2025-08-13 16:01:35 +0530

    add product by id api

--- backend/controllers/product-controller.js
+++ backend/controllers/product-controller.js
@@ -12,3 +12,16 @@ export const createProduct = async (req, res) => {
   const product = await Product.create(req.body.product);
   return res.status(200).json({ product });
 };
+
+// get product by product id /api/v1/products/:id
+export const getProductById = async (req, res) => {
+  const product = await Product.findById(req?.params?.id);
+
+  if (!product) {
+    return res
+      .status(404)
+      .json({ message: `product not found with id ${req?.params?.id}` });
+  }
+
+  return res.status(200).json({ product });
+};
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,10 +2,12 @@ import express from "express";
 import {
   createProduct,
   getAllProducts,
+  getProductById,
 } from "../controllers/product-controller.js";
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/admin/products").post(createProduct);
+router.route("/products/:id").get(getProductById);
 
 export default router;
