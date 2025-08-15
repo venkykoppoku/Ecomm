1dbeb72b8cc84130e872e7f63ccdbf0f945981f2
venkykoppoku <venky94.koppoku@gmail.com>
2025-08-15 17:25:30 +0530

    generate jwt token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,8 +2,9 @@ import catchAsync from "../middlewares/catchAsync.js";
 import User from "../models/user.js";
 
 export const registerUser = catchAsync(async (req, res) => {
-  await User.create(req.body);
+  const user = await User.create(req.body);
+  const token = user.getJwtToken();
   res.status(201).json({
-    success: true,
+    token,
   });
 });
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 const userSchema = mongoose.Schema(
   {
     name: {
@@ -38,4 +39,10 @@ userSchema.pre("save", async function (next) {
   }
   this.password = await bcrypt.hash(this.password, 10);
 });
+
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRETE, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+};
 export default mongoose.model("User", userSchema);
