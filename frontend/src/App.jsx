8877f7aa1981fa22665b998d3032d7e475fd1de4
venkykoppoku <venky94.koppoku@gmail.com>
2025-08-24 19:16:12 +0530

    add initial home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <div className="container">
-        <Home />
+    <Router>
+      <div className="app">
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="py-1 pt-5">
       <p className="text-center mt-1 fw-bold">
-        Shopit 2025-2028, All rights reserved
+        ShopIT - 2020-2023, All Rights Reserved
       </p>
     </footer>
   );
